9abc2f6225fbbabb716a694217fda4b50453b744
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-25 00:06:36 +0100

    get Data from firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
- console.log('hello from index.js');
+import {initializeApp} from 'firebase/app'
+
+import {
+    getFirestore,
+    collection,
+    getDocs
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPw_JVrNqeWpGpDUfWRZ1_bbjfPisF5dc",
+    authDomain: "fir-basics-github-tfk-5387f.firebaseapp.com",
+    projectId: "fir-basics-github-tfk-5387f",
+    storageBucket: "fir-basics-github-tfk-5387f.appspot.com",
+    messagingSenderId: "335320390985",
+    appId: "1:335320390985:web:781a260f2f6d163c1a0e98"
+  };
+
+  initializeApp(firebaseConfig)
+
+
+const db=getFirestore()
+
+const colRef=collection(db,'books')
+
+getDocs(colRef)
+  .then((snapshot)=>{
+     // console.log(snapshot.docs);
+     let books=[]
+      snapshot.docs.forEach((doc)=>{
+          books.push({...doc.data(), id:doc.id})
+      })
+      console.log(books);
+     
+  })
+  .catch(err=>console.log(err.message))
