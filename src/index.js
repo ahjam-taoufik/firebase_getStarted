54e9db831c794423aad33b3bed8e3b42cb2cd018
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-25 00:33:28 +0100

    addData_DeleteData

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import {initializeApp} from 'firebase/app'
 import {
     getFirestore,
     collection,
-    getDocs
+    getDocs,
+    addDoc,
+    deleteDoc,
+    doc
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -33,3 +36,31 @@ getDocs(colRef)
      
   })
   .catch(err=>console.log(err.message))
+
+  // adding docs
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
+
