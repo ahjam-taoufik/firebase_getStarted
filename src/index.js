d244e1697055678496be46bdb8f7e14c1f6d9a32
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-25 12:46:31 +0100

    SignIn_SignOut

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ import {
 
 import {
      getAuth,
-     createUserWithEmailAndPassword
+     createUserWithEmailAndPassword,
+     signOut,
+     signInWithEmailAndPassword
     } from "firebase/auth";
 
 const firebaseConfig = {
@@ -119,3 +121,34 @@ signupForm.addEventListener('submit', (e) => {
       console.log(err.message)
     })
 })
+
+// logging in and out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+
